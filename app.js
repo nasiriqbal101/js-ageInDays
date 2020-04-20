490c8e0d3806f9d7e5e600ed669296ea1e1f5a7f
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-04-20 11:11:25 +0100

    Properly Indented

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
 ageInDays = () => {
-let birthYear = prompt('what is your birth year?');
-let yourAgeInDays = (2020 - birthYear) * 365;
-let h1 = document.createElement('h1');
-let textAnswer = document.createTextNode('You are ' + yourAgeInDays + ' days old');
-h1.setAttribute('id', 'ageInDays');
-h1.appendChild(textAnswer);
-document.getElementById('flex-result').appendChild(h1);
-console.log(yourAgeInDays);
-}
+    let birthYear = prompt('what is your birth year?');
+    let yourAgeInDays = (2020 - birthYear) * 365;
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode('You are ' + yourAgeInDays + ' days old');
 
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+
+    document.getElementById('flex-result').appendChild(h1);
+}
 reset = () => {
     document.getElementById('ageInDays').remove();
 }
